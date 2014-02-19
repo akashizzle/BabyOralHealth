a962aaedffe61856fdce9fa464c98602f0e5f225
Mitchell Au <aumitche@gmail.com>
2014-02-18 20:23:58 -0500

    java file for dentist page

---
+++ src/com/example/babyoralhealth/DentistActivity.java
@@ -0,0 +1,27 @@
+package com.example.babyoralhealth;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class DentistActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dentist);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.age, menu);
+		return true;
+	}
+	
+	public void goBack(View view){
+		onBackPressed();
+	}
+
+}
